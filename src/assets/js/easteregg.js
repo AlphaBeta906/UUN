abd69968a8032e9c69931947c0f92bbded5e788c
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-10-04 13:02:35 +0800

    feat: remove uneeded complexity

--- src/assets/js/easteregg.js
+++ src/assets/js/easteregg.js
@@ -1,32 +1,21 @@
 import figlet from 'figlet'
-import standard from 'figlet/importable-fonts/Standard.js'
+import O8 from 'figlet/importable-fonts/O8'
 
-figlet.parseFont('Standard', standard);
+figlet.parseFont('Standard', O8);
 
-class devtools {
-    constructor() { }
-    static async toString() {
-        if (!this.opened) {
-            console.clear()
-            
-            figlet.text('UUN', {
-                font: 'Standard',
-            }, function (err, data) {
-                if (err) {
-                    console.log('Something went wrong...');
-                    console.dir(err);
-                    return;
-                }
-                
-                console.log(`%c${data}`, ['font-weight: 900', 'font-family: monospace','color: #5b92e5'].join(';'));
-                console.log(`Welcome, %c-ACheesyNameHere-`, ['color: black', 'background: black', 'border: 1px solid black'].join(';'));
-            });
-        }
-        this.opened = true;
+
+figlet.text('UUN', {
+    font: 'Standard',
+}, function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
     }
-}
 
-console.log("%c", devtools)
+    console.log(`%c${data}`, ['font-weight: 900', 'font-family: monospace', 'color: #5b92e5'].join(';'));
+    console.log(`Welcome, %c-ACheesyNameHere-`, ['color: black', 'background: black', 'border: 1px solid black'].join(';'));
+});
 
 console.warn = function () { }
 console.error = function () { }
