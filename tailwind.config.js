8a7d6619dccfc3c967e2fcb68234d47ca5cb8593
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-10-11 11:13:20 +0800

    fix: link colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
       "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     ],
     plugins: [require("daisyui")],
+    important: true,
     daisyui: {
-        themes: ["light"]
+      themes: ["light"]
     }
 }
   
