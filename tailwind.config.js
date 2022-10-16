19e0d75ef1026cf2a7b2b96f20a9d51bbc553229
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-10-16 09:27:15 +0800

    feat: updated header/footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    content: [
-      "./src/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug,svelte}",
-    ],
-    plugins: [
-      require("daisyui")
-    ],
-    important: true,
-    darkMode: 'class',
-    daisyui: {
-      themes: ["light", "dark"]
-    }
+  content: [
+    "./src/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug,svelte}",
+  ],
+  plugins: [
+    require("daisyui")
+  ],
+  important: true,
+  darkMode: 'class',
+  daisyui: {
+    themes: [
+      "light",
+      "dark"
+    ]
+  }
 }
-  
