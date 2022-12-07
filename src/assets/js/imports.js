18d257c27728a20e9852a6d1250855396ae9a639
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-12-07 15:18:29 +0800

    feat(adsense): ads now are fixed to the bottom and vertically centered (bad fix but will clean up)

--- src/assets/js/imports.js
+++ src/assets/js/imports.js
@@ -1,3 +1,11 @@
 import { themeChange } from 'theme-change';
 
 themeChange()
+
+if (window.localStorage.getItem("theme") === null) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.theme = "dark"
+    } else {
+        localStorage.theme = "light"
+    }
+}
