af0e736728185a07c004fe0ef16210dfc50356f5
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-12-09 08:54:28 +0800

    fix: forgot to add dotenv

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,10 +1,14 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 var google = document.createElement('script');
-google.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${import.meta.env.GOOGLE}`;
+google.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE}`;
 google.async = true;
 google.crossorigin = 'anonymous';
 document.head.appendChild(google);
 
 var client = document.createElement('script');
-client.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${import.meta.env.CLIENT}`;
+client.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.CLIENT}`;
 client.async = true;
 document.head.appendChild(script);
