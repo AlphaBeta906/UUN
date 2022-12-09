79d97f0a5be92c541759d6f435ec3ebe82ca13c1
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-12-09 08:57:52 +0800

    fix: maybe fix for scripts

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -3,12 +3,12 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 var google = document.createElement('script');
-google.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE}`;
+google.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.CLIENT}`;
 google.async = true;
 google.crossorigin = 'anonymous';
 document.head.appendChild(google);
 
 var client = document.createElement('script');
-client.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.CLIENT}`;
+client.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE}`;
 client.async = true;
 document.head.appendChild(script);
