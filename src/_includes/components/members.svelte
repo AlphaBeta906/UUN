6d483e121a20cf28afb4ca0d99597be54262c8d0
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-12-27 21:48:39 +0800

    feat: make member nations work on mobile

---
+++ src/_includes/components/members.svelte
@@ -0,0 +1,85 @@
+<script>
+    let nations = {
+        "Akail": {
+            img: "Unknown.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Alphadonia": {
+            img: "Alphadonia.png",
+            world: "Aztrax",
+            date: "August 27th, 1979"
+        },
+        "Atelia": {
+            img: "Unknown.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Bornia": {
+            img: "Unknown.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Atelia": {
+            img: "Cornia.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Emirate, Empire of": {
+            img: "Emirate.jpg",
+            world: "Aztrax",
+            date: "August 27th, 1979"
+        },
+        "Mawbol": {
+            img: "Mawbol.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Neuster": {
+            img: "Unknown.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Neuver": {
+            img: "Neuver.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Romanov": {
+            img: "Romanov.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        },
+        "Sundir": {
+            img: "Unknown.png",
+            world: "Werbia",
+            date: "August 27th, 1979"
+        }
+    }
+
+    // Create items array
+    var items = Object.keys(nations).map(function(key) {
+        return [key, nations[key]];
+    });
+
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+</script>
+
+<div class="grid grid-cols-3 gap-20">
+    {#each items as [key, value]}
+        <div class="card w-100 sm:w-70 bg-base-300 shadow-xl">
+            <div class="card-body text-xs md:text-base">
+                <h2 class="card-title p-0 pb-2"><img src={`/assets/img/flags/${value["img"]}`} alt='Flag for a nation' class="md:h-[22.5px]" /> <span class="hidden md:block">{key}</span></h2>
+                <p class="italic">
+                    <span class="block md:hidden font-bold">{key}</span><br><br>
+
+                    <b>World of Origin</b>: {value["world"]}<br>
+                    <b>Date of Admission</b>: {value["date"]}
+                </p>
+            </div>
+        </div>
+    {/each}
+</div>
