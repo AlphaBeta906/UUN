0deab09ee92d41929ca0b1639ee569564dac9ba0
AlphaBeta906 <dragonkingalyx@gmail.com>
2023-01-04 21:23:42 +0800

    feat: make members page sortable

--- src/_includes/components/members.svelte
+++ src/_includes/components/members.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { flip } from 'svelte/animate';
+
     let nations = {
         "Akail": {
             img: "Unknown.png",
@@ -66,11 +68,21 @@
     items.sort(function(first, second) {
         return second[1] - first[1];
     });
+
+    function reverse() {
+        items = items.reverse()
+    }
 </script>
 
+<center>
+    <button on:click={reverse} class="btn mb-5 md:btn-lg">
+        Reverse
+    </button>
+</center>
+
 <div class="grid grid-cols-3 gap-20">
-    {#each items as [key, value]}
-        <div class="card w-100 sm:w-70 bg-base-300 shadow-xl text-black dark:text-white">
+    {#each items as [key, value] (key)}
+        <div animate:flip={{ delay: 250, duration: 500 }} class="card w-100 sm:w-70 bg-base-300 shadow-xl text-black dark:text-white">
             <div class="card-body text-xs md:text-base">
                 <h2 class="card-title p-0 pb-2"><img src={`/flags/${value["img"]}`} alt='Flag for a nation' class="md:h-[22.5px]" /> <span class="hidden md:block">{key}</span></h2>
                 <p class="italic">
